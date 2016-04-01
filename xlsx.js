5a0cf49b2cbe357e469a0a132c784e734228e5f3
MKImaduddin <mk.imaduddin@gmail.com>
2016-04-01 18:07:15 +0700

    add stub & blank fix to xlsx.js

--- xlsx.js
+++ xlsx.js
@@ -7241,8 +7241,7 @@ function safe_format(p, fmtid, fillid, opts) {
 				if((dd|0) === dd) p.w = SSF._general_int(dd,_ssfopts);
 				else p.w = SSF._general_num(dd,_ssfopts);
 			}
-			else if(p.v === undefined) return "";
-			else p.w = SSF._general(p.v,_ssfopts);
+			else if(p.v !== undefined) p.w = SSF._general(p.v,_ssfopts);
 		}
 		else if(p.t === 'd') p.w = SSF.format(fmtid,datenum(p.v),_ssfopts);
 		else p.w = SSF.format(fmtid,p.v,_ssfopts);
@@ -11552,6 +11551,7 @@ function sheet_to_json(sheet, opts){
 				case 'e': continue;
 				case 's': break;
 				case 'b': case 'n': break;
+				case 'stub': break;
 				default: throw 'unrecognized type ' + val.t;
 			}
 			if(v !== undefined) {
